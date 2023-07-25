0e28977a04c4e625ddad733e4ecbebea606da837
Carter Comish <carter.comish@rocscience.com>
2023-07-25 09:49:23 -0400

    added comments

--- generateAndBuildDocumentation.py
+++ generateAndBuildDocumentation.py
@@ -1,7 +1,16 @@
 import subprocess
 
+"""
+sphinx-apidoc Documentation: https://www.sphinx-doc.org/en/master/man/sphinx-apidoc.html
+sphinx-build Documentation: https://www.sphinx-doc.org/en/master/man/sphinx-build.html
+"""
+
 def run_sphinx_apidoc():
     # Command to run sphinx-apidoc
+    # sphinx-apidoc [OPTIONS] -o <OUTPUT_PATH> <MODULE_PATH> [EXCLUDE_PATTERN …]
+    # --force: Force overwriting of any existing generated files.
+    # .py files listed are ignored from documentation generation
+
     cmd = [
         "sphinx-apidoc",
         "--force",
@@ -17,7 +26,15 @@ def run_sphinx_apidoc():
 
 def run_sphinx_build():
     # Command to run sphinx-build
-    cmd = ["sphinx-build", "-b", "html", "docs", "docs/_build/html"]
+    # sphinx-build [options] <sourcedir> <outputdir> [filenames …]
+
+    cmd = [
+        "sphinx-build", 
+        "-b", 
+        "html", 
+        "docs", 
+        "docs/_build/html"
+        ]
     subprocess.run(cmd, check=True)
 
 if __name__ == "__main__":
