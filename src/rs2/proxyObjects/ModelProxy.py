55225970326baa2de6333ee11b521a7cfd1e3c4c
Carter Comish <carter.comish@rocscience.com>
2023-08-18 11:05:17 -0400

    fix docstring

--- src/rs2/proxyObjects/ModelProxy.py
+++ src/rs2/proxyObjects/ModelProxy.py
@@ -52,13 +52,13 @@ class ModelProxy(ProxyObject):
 	
 	def compute(self):
 		'''
-		Saves the file and then Runs compute. Replaces any existing results.
+		Saves the file if modified and then runs compute. Replaces any existing results.
 		'''
 		return self._callFunction('compute', [False])
 
 	def computeGroundWater(self):
 		'''
-		Saves the file and then Runs groundwater compute. Replaces any existing results.
+		Saves the file if modified and then runs groundwater compute. Replaces any existing results.
 		'''
 		return self._callFunction('compute', [True])
 
