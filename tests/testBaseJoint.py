336972a3b5be6ee2022a438d40e986f838d43049
William Sati <william.sati@rocscience.com>
2023-10-02 11:51:35 -0400

    fix test values to use random seed

--- tests/testBaseJoint.py
+++ tests/testBaseJoint.py
@@ -21,8 +21,8 @@ class TestBaseJoint(unittest.TestCase):
         os.remove(self.copiedModelPath)
     def testBaseJointProperty(self):
         joint = self.joint
-        joint.setJointName("QpSv9")
-        joint.setJointColor(17399)
+        joint.setJointName("VYJpH")
+        joint.setJointColor(31891)
         joint.setSlipCriterion(JointTypes.JOINT_HYPERBOLIC_SIMPLE)
         joint.setInitialDeformation(0)
         joint.SetApplySSR(True)
@@ -33,8 +33,8 @@ class TestBaseJoint(unittest.TestCase):
         self.model = self.modeler.openFile(self.copiedModelPath)
         self.joint = self.model.getAllJointProperties()[0]
         joint = self.joint
-        self.assertEqual(joint.getJointName(), "QpSv9")
-        self.assertEqual(joint.getJointColor(), 17399)
+        self.assertEqual(joint.getJointName(), "VYJpH")
+        self.assertEqual(joint.getJointColor(), 31891)
         self.assertEqual(joint.getSlipCriterion(), JointTypes.JOINT_HYPERBOLIC_SIMPLE)
         self.assertEqual(joint.getInitialDeformation(), 0)
         self.assertEqual(joint.GetApplySSR(), True)
