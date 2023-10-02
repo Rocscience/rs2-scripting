dc66b0816580fbb3191bd314584beaced684fffe
William Sati <william.sati@rocscience.com>
2023-10-02 11:38:10 -0400

    add extra functions tests

--- tests/testBaseJoint.py
+++ tests/testBaseJoint.py
@@ -21,16 +21,22 @@ class TestBaseJoint(unittest.TestCase):
         os.remove(self.copiedModelPath)
     def testBaseJointProperty(self):
         joint = self.joint
-        joint.setJointName("3Kmuv")
-        joint.setJointColor(28211)
+        joint.setJointName("QpSv9")
+        joint.setJointColor(17399)
         joint.setSlipCriterion(JointTypes.JOINT_HYPERBOLIC_SIMPLE)
-        joint.setInitialDeformation(1)
+        joint.setInitialDeformation(0)
+        joint.SetApplySSR(True)
+        joint.SetPermeable(True)
+        joint.SetMeshConforming(True)
         self.model.save()
         self.model.close()
         self.model = self.modeler.openFile(self.copiedModelPath)
         self.joint = self.model.getAllJointProperties()[0]
         joint = self.joint
-        self.assertEqual(joint.getJointName(), "3Kmuv")
-        self.assertEqual(joint.getJointColor(), 28211)
+        self.assertEqual(joint.getJointName(), "QpSv9")
+        self.assertEqual(joint.getJointColor(), 17399)
         self.assertEqual(joint.getSlipCriterion(), JointTypes.JOINT_HYPERBOLIC_SIMPLE)
-        self.assertEqual(joint.getInitialDeformation(), 1)
+        self.assertEqual(joint.getInitialDeformation(), 0)
+        self.assertEqual(joint.GetApplySSR(), True)
+        self.assertEqual(joint.GetPermeable(), True)
+        self.assertEqual(joint.GetMeshConforming(), True)
