4587262746aa6f40efe199e2e63d57dd4fc2a68f
William Sati <william.sati@rocscience.com>
2023-10-03 16:41:39 -0400

    add initial application manager

---
+++ sampleClientScripts/SampleApplicationManager.py
@@ -0,0 +1,6 @@
+from rs2.ApplicationManager import ApplicationManager
+
+appManager = ApplicationManager()
+appManager.startApplication(r"C:\Users\WilliamSati\source\repos\RS2\RS2_2018\Build\Debug_x64\RS2.exe", 60057)
+print("Done")
+
---
+++ src/rs2/ApplicationManager.py
@@ -0,0 +1,36 @@
+import subprocess
+import time
+from rs2.Client import Client
+from rsmessages.requestFormat import functionRequest
+
+class ApplicationManager:
+    minimumPort = 49152
+    maximumPort = 65535
+    def startApplication(self, pathToExecutable : str, port : int, timeout : float = None ):
+        """
+        Starts the application specified by pathToExecutable and starts the python server on the given port. 
+        Returns when the server is ready to accept requests.
+        Throws TimeoutError exception if the server is not ready within that time.
+
+        pathToExecutable: the full path to the executable of the application you want to start.
+        port: the port number you want the python server to bind to.
+        timeout: time in seconds before we stop trying to start the application
+        """
+        if port < self.minimumPort | port > self.maximumPort:
+            raise ValueError(f"port must be in the range {self.minimumPort} to {self.maximumPort}")
+        
+        p = subprocess.Popen([f"{pathToExecutable}", "-startpythonserver", f"{port}"], start_new_session = True, creationflags=subprocess.DETACHED_PROCESS)
+        #continuously try to connect to the server and send a message. If it takes longer than timeout, throw a timeout exception
+        startTime = time.time()
+        serverIsRunning = False
+
+        while not serverIsRunning:
+            if timeout:
+                if (time.time() - startTime) > timeout:
+                    raise TimeoutError("The application did not start within the given timeout time.")
+            try:
+                client = Client("localhost", port)
+                client.closeConnection()
+            except Exception as e:
+                continue
+            serverIsRunning = True
