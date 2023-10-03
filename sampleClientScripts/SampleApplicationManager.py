ae78ba637c2e5b8cdee88a3637ba9ba9c1348f06
William Sati <william.sati@rocscience.com>
2023-10-03 18:21:55 -0400

    update sample script

--- sampleClientScripts/SampleApplicationManager.py
+++ sampleClientScripts/SampleApplicationManager.py
@@ -1,6 +1,9 @@
 from rs2.ApplicationManager import ApplicationManager
+from rs2.RS2Modeler import RS2Modeler
+from rs2.PropertyEnums import *
 
 appManager = ApplicationManager()
 appManager.startApplication(r"C:\Users\WilliamSati\source\repos\RS2\RS2_2018\Build\Debug_x64\RS2.exe", 60057)
-print("Done")
 
+modeler = RS2Modeler(port=60057)
+model = modeler.openFile(r"C:\scriptingModels\simple_3_stage.fez")
