4d2ab26efe441986587c2fcf9c1f3cebc3910059
William Sati <william.sati@rocscience.com>
2023-10-04 11:03:54 -0400

    add static start to modeler

---
+++ sampleClientScripts/SampleStartModeler.py
@@ -0,0 +1,6 @@
+from rs2.RS2Modeler import RS2Modeler
+from rs2.PropertyEnums import *
+
+RS2Modeler.startApplication(60057, r"C:\Users\WilliamSati\source\repos\RS2\RS2_2018\Build\Debug_x64\RS2.exe")
+modeler = RS2Modeler(port=60057)
+model = modeler.openFile(r"C:\scriptingModels\simple_3_stage.fez")
--- src/rs2/ApplicationManager.py
+++ src/rs2/ApplicationManager.py
@@ -1,7 +1,6 @@
 import subprocess
 import time
 from rs2.Client import Client
-from rsmessages.requestFormat import functionRequest
 
 class ApplicationManager:
     minimumPort = 49152
@@ -12,9 +11,14 @@ class ApplicationManager:
         Returns when the server is ready to accept requests.
         Throws TimeoutError exception if the server is not ready within that time.
 
-        pathToExecutable: the full path to the executable of the application you want to start.
-        port: the port number you want the python server to bind to.
-        timeout: time in seconds before we stop trying to start the application
+        Args:
+            pathToExecutable: the full path to the executable of the application you want to start.
+            port: the port number you want the python server to bind to.
+            timeout: time in seconds before we stop trying to start the application
+        
+        Raises:
+        	ValueError: Port range must be between 49152 and 65535, otherwise ValueError is raised
+		    TimeoutError: if timeout is provided, raises TimeoutError if not able to connect to the server within that time.
         """
         if port < self.minimumPort | port > self.maximumPort:
             raise ValueError(f"port must be in the range {self.minimumPort} to {self.maximumPort}")
