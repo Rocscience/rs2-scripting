6911c5a7bd1d4903cb34212830845cf15c63827d
William Sati <william.sati@rocscience.com>
2023-10-12 15:02:49 -0400

    fix gitignore

--- sampleClientScripts/DefaultConstants.py
+++ sampleClientScripts/DefaultConstants.py
@@ -1,5 +1,5 @@
 """
-Provide your own definition of these contants in a file called LocalPaths.py
+Provide your own definition of these contants in a file called LocalConstants.py
 """
 modelerDebugPath = r"C:\Users\WilliamSati\source\repos\RS2\RS2_2018\Build\Debug_x64\RS2.exe"
 interpreterDebugPath = r"C:\Users\WilliamSati\source\repos\RS2\RS2_2018\Build\Debug_x64\Interpret.exe"
