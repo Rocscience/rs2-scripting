d1fdd31e82487525b1a905806257b2c806374026
William Sati <william.sati@rocscience.com>
2023-10-12 15:01:28 -0400

    delete LocalConstants

--- sampleClientScripts/LocalConstants.py
+++
@@ -1,5 +0,0 @@
-modelerDebugPath = r"C:\Users\WilliamSati\source\repos\RS2\RS2_2018\Build\Debug_x64\RS2.exe"
-interpreterDebugPath = r"C:\Users\WilliamSati\source\repos\RS2\RS2_2018\Build\Debug_x64\Interpret.exe"
-
-modelerPort = 60057
-interpreterPort = 60058
