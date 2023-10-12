9456a7e76b378e72efe0736a4ab063aa3de207d9
William Sati <william.sati@rocscience.com>
2023-10-12 11:27:32 -0400

    add Paths module

---
+++ sampleClientScripts/Paths.py
@@ -0,0 +1,9 @@
+#Add the paths to these models on your computer. The models can be found in S:\willSati\Scripting\TestModels 
+#and should be copied to your drive.
+modelerDebugPath = ""
+simple3StageModelPath = ""
+computationWarningModelPath = ""
+staticThermalModelPath = ""
+boltAndMaterialsModelPath = ""
+profilesAndBoreholesModelPath = ""
+simpleProbabilisticModelPath = ""
