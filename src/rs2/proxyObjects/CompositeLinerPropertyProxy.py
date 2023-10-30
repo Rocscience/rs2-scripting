c73fa97379ef738411a4ba3313bb9155f5d5bf1b
aakash.vaithyanathan <aakash.vaithyanathan@rocscience.com>
2023-10-30 15:08:13 -0400

    Functions to test and expose composite liners

---
+++ src/rs2/proxyObjects/CompositeLinerPropertyProxy.py
@@ -0,0 +1,81 @@
+from rs2.proxyObjects.propertyProxy import PropertyProxy
+from rs2.Client import Client
+from enum import Enum, auto
+from typing import List
+from rs2.PropertyEnums import *
+class CompositeLinerProperty(PropertyProxy):
+	"""
+	:ref:`Composite Liner Example`
+	"""
+	def getCompositeName(self) -> str:
+		return self._getCStringProperty("CLP_NAME")
+	def setCompositeName(self, value: str):
+		return self._setCStringProperty("CLP_NAME", value)
+	def getCompositeColor(self) -> int:
+		return self._getUnsignedLongProperty("CLP_COLOR")
+	def setCompositeColor(self, value: int):
+		return self._setUnsignedLongProperty("CLP_COLOR", value)
+	def getJointPlacement(self) -> CompositeJointPlacementTypes:
+		return CompositeJointPlacementTypes(self._getEnumECompositeJointPlacementProperty("CLP_JOINT_PLACEMENT"))
+	def setJointPlacement(self, value: CompositeJointPlacementTypes):
+		return self._setEnumECompositeJointPlacementProperty("CLP_JOINT_PLACEMENT", value)
+	def getJointPropertyName(self) -> str:
+		"""
+		Returns the applied joint name
+		"""
+		return self._callFunction("getJointPropertyName", [])
+	def setJointPropertyByName(self, jointName: str):
+		"""
+		Set joint by name
+		"""
+		return self._callFunction("setJointPropertyByName", [jointName])
+	def getLinerPropertyName(self, layerNumber: int) -> str:
+		"""
+		Returns the liner name for specified layer number
+		"""
+		return self._callFunction("getLinerPropertyName", [layerNumber])
+	def setLinerPropertyByName(self, layerNumber: int, linerName: str):
+		"""
+		Set liner by name for specified layer number
+		"""
+		return self._callFunction("setLinerPropertyByName", [layerNumber, linerName])
+	def getNumberOfLayers(self) -> int:
+		"""
+		Returns number of layers
+		"""
+		return self._callFunction("getNumberOfLayers", [])
+	def setNumberOfLayers(self, num_layers: int):
+		"""
+		Set number of layers
+		"""
+		return self._callFunction("setNumberOfLayers", [num_layers])
+	def getJointApplied(self) -> bool:
+		"""
+		Returns boolean indicating whether joint interface is applied or not
+		"""
+		return self._callFunction("getJointApplied", [])
+	def setJointApplied(self, joint_applied: bool):
+		"""
+		Set joint interface as boolean
+		"""
+		return self._callFunction("setJointApplied", [joint_applied])
+	def getInstallDelay(self, layerNumber: int) -> int:
+		"""
+		Returns install delay as integer for specified layer number
+		"""
+		return self._callFunction("getInstallDelay", [layerNumber])
+	def setInstallDelay(self, layerNumber: int, stagesBelow: int):
+		"""
+		Set install delay for specified layer number
+		"""
+		return self._callFunction("setInstallDelay", [layerNumber, stagesBelow])
+	def getRemovedStage(self, layerNumber: int) -> int:
+		"""
+		Returns removed stages as integer for specified layer number
+		"""
+		return self._callFunction("getRemovedStage", [layerNumber])
+	def setRemovedStage(self, layerNumber: int, stagesBelow: int):
+		"""
+		Set removed stages for specified layer number
+		"""
+		return self._callFunction("setRemovedStage", [layerNumber, stagesBelow])
