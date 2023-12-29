9ce0949e76b3ea9d5b6657c5fd7b5c0f84fb0ba9
William Sati <william.sati@rocscience.com>
2023-12-29 11:57:02 -0500

    add userDefined and CustomWaterContent functions

--- src/rs2/proxyObjects/MaterialSubproxyObjects/HydraulicSubProxyObjects/FEAGroundwaterProxies/UserDefined.py
+++ src/rs2/proxyObjects/MaterialSubproxyObjects/HydraulicSubProxyObjects/FEAGroundwaterProxies/UserDefined.py
@@ -4,18 +4,7 @@ from enum import Enum, auto
 from typing import List
 from rs2.PropertyEnums import *
 class UserDefined(PropertyProxy):
-	def getWCInputType(self) -> WCInputType:
-		return WCInputType(self._getEnumEWCInputTypeProperty("MP_WC_INPUT_TYPE"))
-	def setWCInputType(self, value: WCInputType):
-		return self._setEnumEWCInputTypeProperty("MP_WC_INPUT_TYPE", value)
 	def setUserDefinedPermeabilityAndWaterContentFunction(self, functionName: str):
 		return self._callFunction("setUserDefinedPermeabilityAndWaterContentFunction", [functionName])
 	def getUserDefinedPermeabilityAndWaterContentFunction(self) -> str:
 		return self._callFunction("getUserDefinedPermeabilityAndWaterContentFunction", [])
-	def setProperties(self, WCInputType : WCInputType = None):
-		if WCInputType is not None:
-			self._setEnumEWCInputTypeProperty("MP_WC_INPUT_TYPE", WCInputType)
-	def getProperties(self):
-		return {
-		"WCInputType" : self.getWCInputType(), 
-		}
