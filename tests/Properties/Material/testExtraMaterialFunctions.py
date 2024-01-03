1881711ed7b870c563b107f901bdae8d360e6a18
William Sati <william.sati@rocscience.com>
2024-01-03 11:34:10 -0500

    add initial test file

---
+++ tests/Properties/Material/testExtraMaterialFunctions.py
@@ -0,0 +1,28 @@
+import unittest
+import os, sys, inspect
+import shutil
+import parentDirectoryHelper
+from rs2.RS2Modeler import RS2Modeler
+from rs2.PropertyEnums import*
+
+parentDirectoryHelper.addParentDirectoryToPath()
+
+class TestMaterialExtraFunctions(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        parentDirectory = parentDirectoryHelper.getParentDirectory()
+        blankModelPath = f"{parentDirectory}/resources/starterProject.fez"
+        self.copiedModelPath = f"{parentDirectory}/resources/testProject.fez"
+        shutil.copy(blankModelPath, self.copiedModelPath)
+        self.modeler = RS2Modeler()
+        self.model = self.modeler.openFile(self.copiedModelPath)
+        self.material = self.model.getAllMaterialProperties()[0]
+
+    @classmethod
+    def tearDownClass(self):
+        self.model.close()
+        self.modeler.client.closeConnection()
+        os.remove(self.copiedModelPath)
+
+    def testCustomStiffnessLoadingTable(self):
+        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable([(),()])
