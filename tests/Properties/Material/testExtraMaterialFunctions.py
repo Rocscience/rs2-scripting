a5b2d302ac0f2ae99b636fced7a3987e435f90bb
William Sati <william.sati@rocscience.com>
2024-01-04 10:48:26 -0500

    add extra function tests

--- tests/Properties/Material/testExtraMaterialFunctions.py
+++ tests/Properties/Material/testExtraMaterialFunctions.py
@@ -7,7 +7,7 @@ from rs2.PropertyEnums import*
 
 parentDirectoryHelper.addParentDirectoryToPath()
 
-class TestMaterialExtraFunctions(unittest.TestCase):
+class TestStiffnessTables(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         parentDirectory = parentDirectoryHelper.getParentDirectory()
@@ -17,6 +17,7 @@ class TestMaterialExtraFunctions(unittest.TestCase):
         self.modeler = RS2Modeler()
         self.model = self.modeler.openFile(self.copiedModelPath)
         self.material = self.model.getAllMaterialProperties()[0]
+        self.material.Stiffness.setElasticType(MaterialElasticityTypes.CUSTOM_STIFFNESS)
 
     @classmethod
     def tearDownClass(self):
@@ -24,5 +25,70 @@ class TestMaterialExtraFunctions(unittest.TestCase):
         self.modeler.client.closeConnection()
         os.remove(self.copiedModelPath)
 
-    def testCustomStiffnessLoadingTable(self):
-        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable([(),()])
+    def testCustomStiffnessLoadingTableSuccess(self):
+        self.material.Stiffness.Custom.setCustomMode(CustomMode.CUSTOM_P)
+        self.assertEqual(self.material.Stiffness.Custom.getCustomMode(), CustomMode.CUSTOM_P)
+
+        newTable = [(1,2,0.3),(4,5,0.4)]
+        self.material.Stiffness.Custom.setUseConstantPoissonsRatio(False)
+        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, newTable)
+        self.assertEqual(self.material.Stiffness.Custom.getCustomStiffnessLoadingTable(), newTable)
+    
+        #if constant poisson ratio is used, then the poisson ratio column should be zeroed out.
+        newTableZeroPoisson = [(1,2,0),(4,5,0)]
+        self.material.Stiffness.Custom.setUseConstantPoissonsRatio(True)
+        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, newTable)
+        self.assertEqual(self.material.Stiffness.Custom.getCustomStiffnessLoadingTable(), newTableZeroPoisson)
+
+        #should be able to set for another type of custom mode
+        newTable2 = [(2,3,0.4),(5,6,0.45)]
+        self.material.Stiffness.Custom.setUseConstantPoissonsRatio(False)
+        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_Q, newTable2)
+
+        self.assertEqual(self.material.Stiffness.Custom.getCustomStiffnessLoadingTable(), newTableZeroPoisson)
+        self.material.Stiffness.Custom.setCustomMode(CustomMode.CUSTOM_Q)
+        self.assertEqual(self.material.Stiffness.Custom.getCustomStiffnessLoadingTable(), newTable2)
+
+    def testCustomStiffnessLoadingTableValidationFailure(self):
+        self.material.Stiffness.Custom.setCustomMode(CustomMode.CUSTOM_P)
+        self.assertEqual(self.material.Stiffness.Custom.getCustomMode(), CustomMode.CUSTOM_P)
+
+        #too few rows
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.3,4)])
+        
+        #non-increasing first column
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.3),(1,5,0.4)])
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_EPSV, [(1,2,0.3),(0.4,5,0.4)])
+            
+        #invalid mode parameter
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_Q, [(-1,2,0.3),(4,5,0.4)])
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_GAMMA, [(-1,2,0.3),(4,5,0.4)])
+
+        #invalid youngs modulus parameter
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,-2,0.3),(4,5,0.4)])
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.3),(4,-5,0.4)])
+        
+        #invalid poisson ratio parameter
+        self.material.Stiffness.Custom.setUseConstantPoissonsRatio(False)
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,-0.3),(4,5,0.4)])
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.3),(4,5,-0.4)])
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.5),(4,5,0.4)])
+        with self.assertRaises(Exception):
+            self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.3),(4,5,0.5)])
+
+        #invalid poisson ratio with constant poisson ratio
+        self.material.Stiffness.Custom.setUseConstantPoissonsRatio(True)
+        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,-0.3),(4,5,0.4)])
+        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.3),(4,5,-0.4)])
+        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.5),(4,5,0.4)])
+        self.material.Stiffness.Custom.setCustomStiffnessLoadingTable(CustomMode.CUSTOM_P, [(1,2,0.3),(4,5,0.5)])
