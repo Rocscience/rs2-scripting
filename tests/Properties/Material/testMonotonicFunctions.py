55e0b71f381f21930e39d67f2cc21e457aa2eb2c
William Sati <william.sati@rocscience.com>
2024-01-04 12:32:49 -0500

    add initial monotonic test file

---
+++ tests/Properties/Material/testMonotonicFunctions.py
@@ -0,0 +1,25 @@
+import unittest
+import os, sys, inspect
+import shutil
+import parentDirectoryHelper
+from rs2.RS2Modeler import RS2Modeler
+from rs2.PropertyEnums import*
+
+parentDirectoryHelper.addParentDirectoryToPath()
+
+class TestMonotonicFunctions(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        parentDirectory = parentDirectoryHelper.getParentDirectory()
+        blankModelPath = f"{parentDirectory}/resources/starterProject.fez"
+        self.copiedModelPath = f"{parentDirectory}/resources/testProject.fez"
+        shutil.copy(blankModelPath, self.copiedModelPath)
+        self.modeler = RS2Modeler()
+        self.model = self.modeler.openFile(self.copiedModelPath)
+        self.material = self.model.getAllMaterialProperties()[0]
+
+    @classmethod
+    def tearDownClass(self):
+        self.model.close()
+        self.modeler.client.closeConnection()
+        os.remove(self.copiedModelPath)
