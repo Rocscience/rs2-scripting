f82ce5251e7b19e9ff976006beddcf1a12655606
William Sati <william.sati@rocscience.com>
2024-01-04 16:43:54 -0500

    start monotonic function tests

--- tests/Properties/Material/testMonotonicFunctions.py
+++ tests/Properties/Material/testMonotonicFunctions.py
@@ -17,9 +17,25 @@ class TestMonotonicFunctions(unittest.TestCase):
         self.modeler = RS2Modeler()
         self.model = self.modeler.openFile(self.copiedModelPath)
         self.material = self.model.getAllMaterialProperties()[0]
+        self.material.Strength.setUnsaturatedBehavior(UnsaturatedParameterType.UNSATURATED_SINGLE_EFFECTIVE_STRESS)
+        self.material.Strength.setSingleEffectiveStressMethod(UnsaturatedSingleEffectiveStressMethod.UNSATURATED_TABULAR_VALUE)
 
     @classmethod
     def tearDownClass(self):
         self.model.close()
         self.modeler.client.closeConnection()
         os.remove(self.copiedModelPath)
+
+    def testUnsaturatedZoneTable(self):
+        self.material.Strength.setTabularValues(UnsaturatedTabularValueMethod.UNSATURATED_RESPECT_DEGREE_SATURATION)
+        newTableCol1 = [-1.1,0,1,2]
+        newTableCol2 = [-1.2,0.2,1.3,2.4]
+        self.material.Strength.setUnsaturatedZoneTable(newTableCol1, newTableCol2)
+        self.material.Strength.getUnsaturatedZoneTable()
+    def testThermalConductivityTable(self):
+        pass
+    def testVolumetricHeatCapacityTable(self):
+        pass
+    def testTemperatureVsUnfrozenWaterContentValues(self):
+        pass
+    def test
