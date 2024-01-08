7d30a8318d6d20187087f14fb6cf8eb826f6045f
William Sati <william.sati@rocscience.com>
2024-01-08 13:19:41 -0500

    expose set shear function material

--- src/rs2/proxyObjects/MaterialSubproxyObjects/StrengthSubProxyObjects/ShearNormalFunction.py
+++ src/rs2/proxyObjects/MaterialSubproxyObjects/StrengthSubProxyObjects/ShearNormalFunction.py
@@ -8,6 +8,10 @@ class ShearNormalFunction(PropertyProxy):
 		return self._getBoolProperty("MP_APPLY_SSR")
 	def setApplySSRShearStrengthReduction(self, value: bool):
 		return self._setBoolProperty("MP_APPLY_SSR", value)
+	def setShearNormalFunctionByName(self, name: str):
+		return self._callFunction("setShearNormalFunctionByName", [name])
+	def getShearNormalFunctionName(self) -> str:
+		return self._callFunction("getShearNormalFunctionName", [])
 	def setProperties(self, ApplySSRShearStrengthReduction : bool = None):
 		if ApplySSRShearStrengthReduction is not None:
 			self._setBoolProperty("MP_APPLY_SSR", ApplySSRShearStrengthReduction)
