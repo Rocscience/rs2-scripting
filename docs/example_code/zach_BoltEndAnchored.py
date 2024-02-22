1f759292b0e45f4d39a3e70b04ed9dc55e5b98c5
Zachary Brookes <zachary.brookes@rocscience.com>
2024-02-22 14:50:02 -0500

    -completed bolt tests

--- docs/example_code/zach_BoltEndAnchored.py
+++ docs/example_code/zach_BoltEndAnchored.py
@@ -9,32 +9,42 @@ boltList = model.getAllBoltProperties()
 bolt1 = boltList[0]
 bolt2 = boltList[1]
 
+def test1():
+    bolt1.setBoltType(BoltTypes.END_ANCHORED)
+    bolt1.EndAnchored.setBoltDiameter(1.1)
+    bolt1.EndAnchored.setBoltModulusE(1.2)
+    bolt1.EndAnchored.setTensileCapacity(1.43)
+    bolt1.EndAnchored.setResidualTensileCapacity(1.4)
+    bolt1.EndAnchored.setOutofPlaneSpacing(1.5) 
+    bolt1.EndAnchored.setPreTensioningForce(1.6)
+    bolt1.EndAnchored.setConstantPretensioningForceInInstallStage(True)
 
-bolt1.setBoltType(BoltTypes.END_ANCHORED)
-bolt1.EndAnchored.setBoltDiameter(1.1)
-bolt1.EndAnchored.setBoltModulusE(1.2)
-bolt1.EndAnchored.setTensileCapacity(1.43)
-bolt1.EndAnchored.setResidualTensileCapacity(1.4)
-bolt1.EndAnchored.setOutofPlaneSpacing(1.5) 
-bolt1.EndAnchored.setPreTensioningForce(1.6)
-bolt1.EndAnchored.setConstantPretensioningForceInInstallStage(True)
-
-bolt2.setBoltType(BoltTypes.END_ANCHORED)
-bolt2.EndAnchored.setConstantPretensioningForceInInstallStage(False)
-
-
-assert(bolt1.EndAnchored.getBoltDiameter(), 1.1)
-assert(bolt1.EndAnchored.getBoltModulusE(), 1.2) 
-assert(bolt1.EndAnchored.getTensileCapacity(), 1.43)
-assert(bolt1.EndAnchored.getResidualTensileCapacity(), 1.4)
-assert(bolt1.EndAnchored.getOutofPlaneSpacing(), 1.5)
-assert(bolt1.EndAnchored.getPreTensioningForce(), 1.6)
-assert(bolt1.EndAnchored.getConstantPretensioningForceInInstallStage(), True)
-
-assert(bolt2.EndAnchored.getConstantPretensioningForceInInstallStage(), False)
+    assert(bolt1.EndAnchored.getBoltDiameter(), 1.1)
+    assert(bolt1.EndAnchored.getBoltModulusE(), 1.2) 
+    assert(bolt1.EndAnchored.getTensileCapacity(), 1.43)
+    assert(bolt1.EndAnchored.getResidualTensileCapacity(), 1.4)
+    assert(bolt1.EndAnchored.getOutofPlaneSpacing(), 1.5)
+    assert(bolt1.EndAnchored.getPreTensioningForce(), 1.6)
+    assert(bolt1.EndAnchored.getConstantPretensioningForceInInstallStage(), True)
 
 
+def test2():
+    bolt2.setBoltType(BoltTypes.END_ANCHORED)
+    bolt2.EndAnchored.setBoltDiameter(1.1)
+    bolt2.EndAnchored.setBoltModulusE(1.2)
+    bolt2.EndAnchored.setTensileCapacity(1.43)
+    bolt2.EndAnchored.setResidualTensileCapacity(1.4)
+    bolt2.EndAnchored.setOutofPlaneSpacing(1.5) 
+    bolt2.EndAnchored.setPreTensioningForce(1.6)
+    bolt2.EndAnchored.setConstantPretensioningForceInInstallStage(False)
 
+    assert(bolt2.EndAnchored.getBoltDiameter(), 1.1)
+    assert(bolt2.EndAnchored.getBoltModulusE(), 1.2) 
+    assert(bolt2.EndAnchored.getTensileCapacity(), 1.43)
+    assert(bolt2.EndAnchored.getResidualTensileCapacity(), 1.4)
+    assert(bolt2.EndAnchored.getOutofPlaneSpacing(), 1.5)
+    assert(bolt2.EndAnchored.getPreTensioningForce(), 1.6)
+    assert(bolt2.EndAnchored.getConstantPretensioningForceInInstallStage(), False)
 
 
 model.save()
