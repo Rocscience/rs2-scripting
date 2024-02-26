54145d6e68581e7d55c51f23fb9cc417a51ffb19
Zachary Brookes <zachary.brookes@rocscience.com>
2024-02-26 08:41:19 -0500

    -address comment on StageForceDisp

--- docs/example_code/zach_PileStageForceDisp.py
+++ docs/example_code/zach_PileStageForceDisp.py
@@ -22,13 +22,21 @@ def test1():
 
 	pile1.ForceDisplacement.stageFactorInterface.setDefinedStageFactors({2:sf2,4:sf4})
 
+	sf_dict = pile1.ForceDisplacement.stageFactorInterface.getDefinedStageFactors()
+
 	sf1 = pile1.ForceDisplacement.stageFactorInterface.getStageFactor(1)
+	sf2_fin = sf_dict[2]
 	sf3 = pile1.ForceDisplacement.stageFactorInterface.getStageFactor(3)
+	sf4_fin = sf_dict[4]
 
 	assert(sf1.getXFactor(),1)
 	assert(sf1.getXFactor(),1)
+	assert(sf2_fin.getXFactor(),1.1)
+	assert(sf2_fin.getYFactor(), 1.2)
 	assert(sf3.getXFactor(),1.1)
 	assert(sf3.getYFactor(),1.2)
+	assert(sf4_fin.getXFactor(),1.3)
+	assert(sf4_fin.getYFactor(),1.4)
 
 test1()
 
