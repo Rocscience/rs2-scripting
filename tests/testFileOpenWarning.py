25fbc447a2020f6c970a5dec77cbd2a44178b2db
aakash.vaithyanathan <aakash.vaithyanathan@rocscience.com>
2024-03-01 15:48:15 -0500

    Update test and provide sample model file with warning when opened

--- tests/testFileOpenWarning.py
+++ tests/testFileOpenWarning.py
@@ -8,11 +8,9 @@ from rs2.interpreter.InterpreterEnums import *
 parentDirectoryHelper.addParentDirectoryToPath()
 
 class TestFileOpenWarning(unittest.TestCase):
-    # Replace with model with warnings like thermal/log file.
-    pathToModelWithWarning = ""
     def setUp(self):
         parentDirectory = parentDirectoryHelper.getParentDirectory()
-        blankModelPath = TestFileOpenWarning.pathToModelWithWarning
+        blankModelPath = f"{parentDirectory}/resources/modelWithLogFileWarning.fez"
         self.copiedModelPath = f"{parentDirectory}/resources/testProjectWithWarning.fez"
         shutil.copy(blankModelPath, self.copiedModelPath)
         self.interpreter = RS2Interpreter()
@@ -22,11 +20,5 @@ class TestFileOpenWarning(unittest.TestCase):
         os.remove(self.copiedModelPath)
         self.model._client.closeConnection()
 
-    @unittest.skipIf(pathToModelWithWarning == "", "path to model with warnings not found")
     def testFileOpenWarningSuccess(self):
-        self.model = self.interpreter.openFile(TestFileOpenWarning.pathToModelWithWarning)
-
-    @unittest.skipIf(pathToModelWithWarning == "", "path to model with warnings not found")
-    def testFileOpenWarningSetResultTypeOfModelView(self):
-        self.model = self.interpreter.openFile(TestFileOpenWarning.pathToModelWithWarning)
-        self.model.SetResultType(ExportResultType.SOLID_DISPLACEMENT_VERTICAL_DISPLACEMENT)
+        self.model = self.interpreter.openFile(self.copiedModelPath)
