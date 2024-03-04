792c98c311d639faf7965641af24dc00758df08a
Ruici Liu <rachel.liu@rocscience.com>
2024-03-04 11:51:33 -0500

    Test commit space (useless)

--- examples/Anchored sheet pile wall_material prop_bolt.py
+++ examples/Anchored sheet pile wall_material prop_bolt.py
@@ -15,6 +15,7 @@ model = modeler.openFile(rf"{current_dir}\..\ExampleFiles\Anchored Sheet Pile Wa
 # Import the Excel file
 excel_data = pd.ExcelFile (rf"{current_dir}\..\ExampleFiles\data.xlsx") #open Excel file
 
+
 '''
 # Joint Properties
 
