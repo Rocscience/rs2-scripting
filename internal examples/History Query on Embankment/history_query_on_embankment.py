9051547a6f35720090c0d5786261d82810fde371
Daniel Wai <daniel.wai@rocscience.com>
2024-03-12 11:45:06 -0400

    Upate History Query on Embankment to add and plot multiple queries

--- internal examples/History Query on Embankment/history_query_on_embankment.py
+++ internal examples/History Query on Embankment/history_query_on_embankment.py
@@ -15,9 +15,8 @@ output_dir = rf'{current_dir}\Results'
 os.makedirs(output_dir, exist_ok=True)
 
 # Output file path
-output_file_name = 'Embankment Consolidation (History Query).fez'
+output_file_name = 'Embankment Consolidation (History Queries).fez'
 output_path = os.path.join(output_dir, output_file_name)
-
 
 # Add history node to model
 
@@ -31,12 +30,16 @@ model = modeler.openFile(rf'{current_dir}\Embankment Consolidation (Initial).fez
 # Save as a different name
 model.saveAs(output_path)
 
-# Add a history query
-model.AddHistoryQueryPoint(x=55,y=-10,history_query_name='HQ 1')
+# Add history queries
+history_query_names = ['HQ 1', 'HQ 2', 'HQ 3', 'HQ 4', 'HQ 5']
+model.AddHistoryQueryPoint(x=35,y=-8,history_query_name=history_query_names[0])
+model.AddHistoryQueryPoint(x=45,y=-9,history_query_name=history_query_names[1])
+model.AddHistoryQueryPoint(x=55,y=-10,history_query_name=history_query_names[2])
+model.AddHistoryQueryPoint(x=65,y=-11,history_query_name=history_query_names[3])
+model.AddHistoryQueryPoint(x=75,y=-12,history_query_name=history_query_names[4])
 
 # Compute model
 model.compute()
-
 
 # Extract excess pore pressure history from start of stage 2 to end of model
 
@@ -44,39 +47,47 @@ model.compute()
 RS2Interpreter.startApplication(port=60055)
 interpreter = RS2Interpreter(port=60055)
 
-# Extract data
+# Analyze Excess Pore Pressure History
 stage_indices = range(2,9) #stage 2 to 8
 model_results = interpreter.openFile(output_path)
-excess_pore_pressure_time_history_all_stages = model_results.GetHistoryQueryResults('HQ 1', 
-                                     HistoryQueryGraphEnums.HorizontalAxisTypes.TIME, 
-                                     HistoryQueryGraphEnums.VerticalAxisTypes.PORE_PRESSURE, 
-                                     stage_indices)
-
-# Create dataframe
 time_label = 'Time (d)'
 excess_pore_pressure_label = 'Excess Pore Pressure (kPa)'
-data_for_dataframe = []
-for stage_data in excess_pore_pressure_time_history_all_stages.values():
-    for history_query_point in stage_data:
-        # Append the data to the list
-        data_for_dataframe.append([history_query_point.GetHorizontalAxisResult(), history_query_point.GetVerticalAxisResult()])
 
-df_epp = pd.DataFrame(data_for_dataframe, columns=[time_label, excess_pore_pressure_label])
+for history_query_name in history_query_names:
+    # Extract data
+    excess_pore_pressure_time_history_all_stages = model_results.GetHistoryQueryResults(f'{history_query_name}', 
+                                        HistoryQueryGraphEnums.HorizontalAxisTypes.TIME, 
+                                        HistoryQueryGraphEnums.VerticalAxisTypes.PORE_PRESSURE, 
+                                        stage_indices)
 
-# Calculate excess pore pressure using the end of stage 1 (start of stage 2) as the reference stage
-reference_pore_pressure = df_epp[excess_pore_pressure_label].iloc[0]
-df_epp[excess_pore_pressure_label] = df_epp[excess_pore_pressure_label] - reference_pore_pressure
+    # Create dataframe
+    data_for_dataframe = []
+    for stage_data in excess_pore_pressure_time_history_all_stages.values():
+        for history_query_point in stage_data:
+            # Append the data to the list
+            data_for_dataframe.append([history_query_point.GetHorizontalAxisResult(), history_query_point.GetVerticalAxisResult()])
 
-# Export data
-df_epp.to_csv(os.path.join(output_dir, 'Excess Pore Pressure History.csv'), index=False)
+    df_epp = pd.DataFrame(data_for_dataframe, columns=[time_label, excess_pore_pressure_label])
 
-# Plot data
-plt.plot(df_epp[time_label], df_epp[excess_pore_pressure_label])
+    # Calculate excess pore pressure using the end of stage 1 (start of stage 2) as the reference stage
+    reference_pore_pressure = df_epp[excess_pore_pressure_label].iloc[0]
+    df_epp[excess_pore_pressure_label] = df_epp[excess_pore_pressure_label] - reference_pore_pressure
 
+    # Export data
+    df_epp.to_csv(os.path.join(output_dir, rf'{history_query_name}.csv'), index=False)
+
+    # Plot data
+    plt.plot(df_epp[time_label], df_epp[excess_pore_pressure_label], label = rf'{history_query_name}')
+
+# Save plot
 plt.xlabel(time_label)
 plt.ylabel(excess_pore_pressure_label)
 plt.title('Excess Pore Pressure History')
-
+plt.legend()
 plt.savefig(os.path.join(output_dir, 'Excess Pore Pressure History.png'))
+
+# Uncomment the following line to show the plot on the screen
+#plt.show()
+
 
 
