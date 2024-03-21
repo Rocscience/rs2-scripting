1cd62ac98256607653f71e4655df84c87822bb5d
William Sati <william.sati@rocscience.com>
2024-03-20 20:05:30 -0400

    remove release info

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,6 @@ sys.path.insert(0, directory_path)
 project = 'RS2 Scripting Reference Manual'
 copyright = '2023, Rocscience Inc.'
 author = 'Rocscience Inc.'
-release = '0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
