87de70ef9774ae4476c24231a413d54dd5595832
Grace <grace.hu@rocscience.com>
2024-08-16 15:51:49 -0400

    updated the SampleDiscreteFunction.py based on feedback.

--- sampleClientScripts/SampleDiscreteFunction.py
+++ sampleClientScripts/SampleDiscreteFunction.py
@@ -2,8 +2,7 @@ from rs2.modeler import properties
 from rs2.modeler.properties.PropertyEnums import *
 from rs2.modeler.RS2Modeler import RS2Modeler
 from rs2.modeler.properties import *
-import os, inspect
-
+import os
 
 """
 1 create new discrete function with new points
@@ -14,63 +13,45 @@ import os, inspect
 
 modeler = RS2Modeler()
 
-parentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-blankModelPath = f"{parentdir}/../tests/resources/starterProject.fez"
-model = modeler.openFile(blankModelPath)
+relative_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../tests/resources/starterProject.fez")
+model = modeler.openFile(relative_path)
 
 material = model.getMaterialPropertyByName("Material 1")
 
 # Create 2 new discrete function
 model.createNewDiscreteFunction("func1")
 model.createNewDiscreteFunction("func2")
-print(len(model.getDiscreteFunctions()), "discrete function(s) is/are in the list.")
+assert len(model.getDiscreteFunctions()) == 2
 
 
 fun1 = model.getDiscreteFunctionByName("func1")
 # Set some points on the discrete function
-fun1.setPointLocations([(1, 1), (2, 2), (3, 3)])
+POINTLOCATIONS = [(1, 1), (2, 2), (3, 3)]
+fun1.setPointLocations(POINTLOCATIONS)
 # Set the Cu values for those points
-fun1.setPointsC([0.5, 1, 0.2])
-try:
-    assert fun1.getPointLocations() == [(1, 1), (2, 2), (3, 3)]
-    print(fun1.getPointLocations(), "is correct.")
-except:
-    print(fun1.getPointLocations(), "is WRONG.")
-
-try:
-    assert fun1.getPointsC() == [0.5, 1, 0.2]
-    print(fun1.getPointsC(), "is correct.")
-except:
-    print(fun1.getPointsC(), "is WRONG.")
+POINTSC = [0.5, 1, 0.2]
+fun1.setPointsC(POINTSC)
+assert fun1.getPointLocations() == POINTLOCATIONS
+assert fun1.getPointsC() == POINTSC
 
 # Assign the new created discrete function to Material 1
-material.Strength.DiscreteFunction.setSelectedDiscreteFunctionByName("func1")
-try:
-    assert material.Strength.DiscreteFunction.getSelectedDiscreteFunctionName() == "func1"
-    print(material.Strength.DiscreteFunction.getSelectedDiscreteFunctionName(), "is correct.")
-except:
-    print(material.Strength.DiscreteFunction.getSelectedDiscreteFunctionName(), "is WRONG.")
+df = material.Strength.DiscreteFunction
+df.setSelectedDiscreteFunctionByName("func1")
+assert df.getSelectedDiscreteFunctionName() == "func1"
 
 # Set the failure criterion to be discrete function
-material.Strength.setFailureCriterion(StrengthCriteriaTypes.DISCRETE_FUNCTION)
-try:
-    assert material.Strength.getFailureCriterion() == StrengthCriteriaTypes.DISCRETE_FUNCTION
-    print(material.Strength.getFailureCriterion(), "is correct.")
-except:
-    print(material.Strength.getFailureCriterion(), "is WRONG.")
+strength = material.Strength
+strength.setFailureCriterion(StrengthCriteriaTypes.DISCRETE_FUNCTION)
+assert strength.getFailureCriterion() == StrengthCriteriaTypes.DISCRETE_FUNCTION
 
 # Delete one of the new discrete functions
 model.deleteDiscreteFunction("func2")
-print(len(model.getDiscreteFunctions()), "discrete function(s) is/are in the list.")
+assert len(model.getDiscreteFunctions()) == 1
 
 # Set the failure criterion to be Mohr Coulomb
-material.Strength.setFailureCriterion(StrengthCriteriaTypes.MOHR_COULOMB)
-try:
-    assert material.Strength.getFailureCriterion() == StrengthCriteriaTypes.MOHR_COULOMB
-    print(material.Strength.getFailureCriterion(), "is correct")
-except:
-    print(material.Strength.getFailureCriterion(), "is WRONG")
+strength.setFailureCriterion(StrengthCriteriaTypes.MOHR_COULOMB)
+assert strength.getFailureCriterion() == StrengthCriteriaTypes.MOHR_COULOMB
 
 # Delete another discrete function
 model.deleteDiscreteFunction("func1")
-print(len(model.getDiscreteFunctions()), "discrete function(s) is/are in the list.")
+assert len(model.getDiscreteFunctions()) == 0
