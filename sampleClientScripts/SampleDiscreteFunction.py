a36c31340717a75541fb478463f1c2c916598dfd
Grace <grace.hu@rocscience.com>
2024-08-16 15:29:38 -0400

    Add the SampleDiscreteFunction.py to the solution. Set assert functions for testing.

--- sampleClientScripts/SampleDiscreteFunction.py
+++ sampleClientScripts/SampleDiscreteFunction.py
@@ -1,9 +1,8 @@
-from src.rs2.modeler import properties
-from src.rs2.modeler.properties import DiscreteFunction
-from src.rs2.modeler.properties.PropertyEnums import *
-from tests import parentDirectoryHelper
+from rs2.modeler import properties
+from rs2.modeler.properties.PropertyEnums import *
 from rs2.modeler.RS2Modeler import RS2Modeler
 from rs2.modeler.properties import *
+import os, inspect
 
 
 """
@@ -15,8 +14,8 @@ from rs2.modeler.properties import *
 
 modeler = RS2Modeler()
 
-parentDirectory = parentDirectoryHelper.getParentDirectory()
-blankModelPath = f"{parentDirectory}/resources/starterProject.fez"
+parentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+blankModelPath = f"{parentdir}/../tests/resources/starterProject.fez"
 model = modeler.openFile(blankModelPath)
 
 material = model.getMaterialPropertyByName("Material 1")
@@ -24,7 +23,7 @@ material = model.getMaterialPropertyByName("Material 1")
 # Create 2 new discrete function
 model.createNewDiscreteFunction("func1")
 model.createNewDiscreteFunction("func2")
-print(model.getDiscreteFunctions())
+print(len(model.getDiscreteFunctions()), "discrete function(s) is/are in the list.")
 
 
 fun1 = model.getDiscreteFunctionByName("func1")
@@ -32,25 +31,46 @@ fun1 = model.getDiscreteFunctionByName("func1")
 fun1.setPointLocations([(1, 1), (2, 2), (3, 3)])
 # Set the Cu values for those points
 fun1.setPointsC([0.5, 1, 0.2])
-print(fun1.getPointLocations())
-print(fun1.getPointsC())
+try:
+    assert fun1.getPointLocations() == [(1, 1), (2, 2), (3, 3)]
+    print(fun1.getPointLocations(), "is correct.")
+except:
+    print(fun1.getPointLocations(), "is WRONG.")
+
+try:
+    assert fun1.getPointsC() == [0.5, 1, 0.2]
+    print(fun1.getPointsC(), "is correct.")
+except:
+    print(fun1.getPointsC(), "is WRONG.")
 
 # Assign the new created discrete function to Material 1
 material.Strength.DiscreteFunction.setSelectedDiscreteFunctionByName("func1")
-print(material.Strength.DiscreteFunction.getSelectedDiscreteFunctionName())
+try:
+    assert material.Strength.DiscreteFunction.getSelectedDiscreteFunctionName() == "func1"
+    print(material.Strength.DiscreteFunction.getSelectedDiscreteFunctionName(), "is correct.")
+except:
+    print(material.Strength.DiscreteFunction.getSelectedDiscreteFunctionName(), "is WRONG.")
 
 # Set the failure criterion to be discrete function
 material.Strength.setFailureCriterion(StrengthCriteriaTypes.DISCRETE_FUNCTION)
-print(material.Strength.getFailureCriterion())
+try:
+    assert material.Strength.getFailureCriterion() == StrengthCriteriaTypes.DISCRETE_FUNCTION
+    print(material.Strength.getFailureCriterion(), "is correct.")
+except:
+    print(material.Strength.getFailureCriterion(), "is WRONG.")
 
 # Delete one of the new discrete functions
 model.deleteDiscreteFunction("func2")
-print(model.getDiscreteFunctions())
+print(len(model.getDiscreteFunctions()), "discrete function(s) is/are in the list.")
 
 # Set the failure criterion to be Mohr Coulomb
 material.Strength.setFailureCriterion(StrengthCriteriaTypes.MOHR_COULOMB)
-print(material.Strength.getFailureCriterion())
+try:
+    assert material.Strength.getFailureCriterion() == StrengthCriteriaTypes.MOHR_COULOMB
+    print(material.Strength.getFailureCriterion(), "is correct")
+except:
+    print(material.Strength.getFailureCriterion(), "is WRONG")
 
 # Delete another discrete function
 model.deleteDiscreteFunction("func1")
-print(model.getDiscreteFunctions())
+print(len(model.getDiscreteFunctions()), "discrete function(s) is/are in the list.")
