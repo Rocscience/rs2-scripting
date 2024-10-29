13f6e50ab63812fea4a1b4da5862f57d8c8f08bb
Grace <grace.hu@rocscience.com>
2024-10-29 12:45:03 -0400

    add an example for import export properties

---
+++ docs/example_code/import_export_prop_example.py
@@ -0,0 +1,62 @@
+from rs2.modeler.properties.PropertyEnums import *
+from rs2.modeler.RS2Modeler import RS2Modeler
+from rs2.modeler.properties import *
+import os, inspect
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(lambda: None))) 
+RS2Modeler.startApplication(port=60095)
+modeler = RS2Modeler(port=60095)
+model1FilePath = rf"{current_dir}\example_models\ExampleModel.fez"
+model2FilePath = rf"{current_dir}\example_models\EmptyExampleModel_start.fez"
+model2FileSavePath = rf"{current_dir}\example_models\EmptyExampleModel_final.fez"
+
+model1 = modeler.openFile(model1FilePath)
+model2 = modeler.openFile(model2FilePath)
+
+model1Till = model1.getMaterialPropertyByName("Till")
+model2Till = model2.getMaterialPropertyByName("till")
+
+# Not all functions are accessible through the setProperties method. 
+# Consult setProperties method definition in documentation to determine properties available.
+
+model1_initial_conditions = model1Till.InitialConditions.getProperties()
+model2Till.InitialConditions.setProperties(**model1_initial_conditions)
+
+model1_stiffness = model1Till.Stiffness.Isotropic.getProperties()
+model2Till.Stiffness.Isotropic.setProperties(**model1_stiffness)
+
+model1_strength = model1Till.Strength.MohrCoulombStrength.getProperties()
+model2Till.Strength.MohrCoulombStrength.setProperties(**model1_strength)
+
+model1_hydraulic = model1Till.Hydraulic.StaticGroundwater.getProperties()
+model2Till.Hydraulic.StaticGroundwater.setProperties(**model1_hydraulic)
+
+model1_thermal_conductivity = model1Till.Thermal.Conductivity.ConstantConductivity.getProperties()
+model2Till.Thermal.Conductivity.ConstantConductivity.setProperties(**model1_thermal_conductivity)
+
+model1_thermal_heat_capacity = model1Till.Thermal.HeatCapacity.ConstantHeatCapacity.getProperties()
+model2Till.Thermal.HeatCapacity.ConstantHeatCapacity.setProperties(**model1_thermal_heat_capacity)
+
+model1_bolt = model1.getAllBoltProperties()[0]
+model2_bolt = model2.getAllBoltProperties()[0]
+
+model1_bolt_properties = model1_bolt.Swellex.getProperties()
+model2_bolt.setBoltType(BoltTypes.SWELLEX)
+model2_bolt.Swellex.setProperties(**model1_bolt_properties)
+
+model1_liner = model1.getAllLinerProperties()[0]
+model2_liner = model2.getAllLinerProperties()[0]
+
+model1_liner_properties = model1_liner.ReinforcedConcrete.getProperties()
+model2_liner.setLinerType(LinerTypes.REINFORCED_CONCRETE)
+model2_liner.ReinforcedConcrete.setProperties(**model1_liner_properties)
+
+model1_joint = model1.getAllJointProperties()[0]
+model2_joint = model2.getAllJointProperties()[0]
+
+model1_joint_properties = model1_joint.MaterialDependent.getProperties()
+model2_joint.setSlipCriterion(JointTypes.MATERIAL_DEPENDENT)
+model2_joint.MaterialDependent.setProperties(**model1_joint_properties)
+
+model2.saveAs(model2FileSavePath)
+modeler.closeProgram()
---
+++ docs/examples/model/import_export_prop.rst
@@ -0,0 +1,9 @@
+Import Export Properties Script Examples
+===============================================================
+
+Download the `ExampleModel.fez <https://github.com/Rocscience/rs2-scripting/blob/main/docs/example_code/example_models/ExampleModel.fez>`_ and `EmptyExampleModel_start.fez <https://github.com/Rocscience/rs2-scripting/blob/main/docs/example_code/example_models/EmptyExampleModel_start.fez>`_ for this example.
+
+.. _Import Export Material Properties Example:
+.. literalinclude:: ../../example_code/import_export_prop_example.py
+   :language: python
+   :caption: Code Snippet: Import Export Properties
