d9c3790c4d564562de7295f68088340fe7b47f64
Grace <grace.hu@rocscience.com>
2024-10-31 16:26:29 -0400

    - add set stiffness Elasticity type and strength failure criterion before getProperties and setProperties - Import csv files to a dictionary

--- docs/example_code/import_export_prop_example.py
+++ docs/example_code/import_export_prop_example.py
@@ -20,30 +20,64 @@ model2Till = model2.getMaterialPropertyByName("till")
 # Not all functions are accessible through the setProperties and getProperties methods. 
 # Consult setProperties and getProperties method definition in documentation to determine properties available.
 
+# Initial Conditions
 model1_initial_conditions = model1Till.InitialConditions.getProperties()
 model2Till.InitialConditions.setProperties(**model1_initial_conditions)
 
 print("Model 2 Till Initial Conditions")
 print(model2Till.InitialConditions.getProperties())
 
-model1_stiffness = model1Till.Stiffness.Isotropic.getProperties()
-model2Till.Stiffness.Isotropic.setProperties(**model1_stiffness)
+# Stiffness
+elasticType = model1Till.Stiffness.getElasticType()
+model2Till.Stiffness.setElasticType(elasticType)
+
+# More Stiffness Elasticity Types can be added here
+if elasticType == MaterialElasticityTypes.ISOTROPIC:
+    model1_stiffness = model1Till.Stiffness.Isotropic.getProperties()
+    model2Till.Stiffness.Isotropic.setProperties(**model1_stiffness)
+elif elasticType == MaterialElasticityTypes.TRANSVERSELY_ISOTROPIC:
+    model1_stiffness = model1Till.Stiffness.TransverselyIsotropic.getProperties()
+    model2Till.Stiffness.TransverselyIsotropic.setProperties(**model1_stiffness)
+elif elasticType == MaterialElasticityTypes.ORTHOTROPIC:
+    model1_stiffness = model1Till.Stiffness.Orthotropic.getProperties()
+    model2Till.Stiffness.Orthotropic.setProperties(**model1_stiffness)
+elif elasticType == MaterialElasticityTypes.DUNCAN_CHANG_HYPERBOLIC:
+    model1_stiffness = model1Till.Stiffness.NonLinearHyperbolic.getProperties()
+    model2Till.Stiffness.NonLinearHyperbolic.setProperties(**model1_stiffness)
+elif elasticType == MaterialElasticityTypes.NON_LINEAR_ISOTROPIC:
+    model1_stiffness = model1Till.Stiffness.NonLinearIsotropic.getProperties()
+    model2Till.Stiffness.NonLinearIsotropic.setProperties(**model1_stiffness)
+elif elasticType == MaterialElasticityTypes.CUSTOM:
+    model1_stiffness = model1Till.Stiffness.Custom.getProperties()
+    model2Till.Stiffness.Custom.setProperties(**model1_stiffness)
+
 
 print("\nModel 2 Till Stiffness")
 print(model2Till.Stiffness.Isotropic.getProperties())
 
-model1_strength = model1Till.Strength.MohrCoulombStrength.getProperties()
-model2Till.Strength.MohrCoulombStrength.setProperties(**model1_strength)
+# Strength
+failureCriterion = model1Till.Strength.getFailureCriterion()
+model2Till.Strength.setFailureCriterion(failureCriterion)
+
+# More Strength Failure Criteria Types can be added here
+if failureCriterion == StrengthCriteriaTypes.MOHR_COULOMB:
+    model1_strength = model1Till.Strength.MohrCoulombStrength.getProperties()
+    model2Till.Strength.MohrCoulombStrength.setProperties(**model1_strength)
+elif failureCriterion == StrengthCriteriaTypes.HOEK_BROWN:
+    model1_strength = model1Till.Strength.HoekBrown.getProperties()
+    model2Till.Strength.HoekBrown.setProperties(**model1_strength)
 
 print("\nModel 2 Till Strength")
 print(model2Till.Strength.MohrCoulombStrength.getProperties())
 
+# Hydraulic
 model1_hydraulic = model1Till.Hydraulic.StaticGroundwater.getProperties()
 model2Till.Hydraulic.StaticGroundwater.setProperties(**model1_hydraulic)
 
 print("\nModel 2 Till Hydraulic")
 print(model2Till.Hydraulic.StaticGroundwater.getProperties())
 
+# Thermal
 model1_thermal_conductivity = model1Till.Thermal.Conductivity.ConstantConductivity.getProperties()
 model2Till.Thermal.Conductivity.ConstantConductivity.setProperties(**model1_thermal_conductivity)
 
@@ -56,6 +90,7 @@ model2Till.Thermal.HeatCapacity.ConstantHeatCapacity.setProperties(**model1_ther
 print("\nModel 2 Till Thermal Heat Capactiy")
 print(model2Till.Thermal.HeatCapacity.ConstantHeatCapacity.getProperties())
 
+# Bolt
 model1_bolt = model1.getAllBoltProperties()[0]
 model2_bolt = model2.getAllBoltProperties()[0]
 
@@ -66,6 +101,7 @@ model2_bolt.Swellex.setProperties(**model1_bolt_properties)
 print("\nModel 2 Bolt Properties")
 print(model2_bolt.Swellex.getProperties())
 
+# Liner
 model1_liner = model1.getAllLinerProperties()[0]
 model2_liner = model2.getAllLinerProperties()[0]
 
@@ -76,6 +112,7 @@ model2_liner.ReinforcedConcrete.setProperties(**model1_liner_properties)
 print("\nModel 2 Liner Properties")
 print(model2_liner.ReinforcedConcrete.getProperties())
 
+# Joint
 model1_joint = model1.getAllJointProperties()[0]
 model2_joint = model2.getAllJointProperties()[0]
 
@@ -86,6 +123,7 @@ model2_joint.MaterialDependent.setProperties(**model1_joint_properties)
 print("\nModel 2 Joint Properties")
 print(model2_joint.MaterialDependent.getProperties())
 
+# Pile
 model1_pile = model1.getAllPileProperties()[0]
 model2_pile = model2.getAllPileProperties()[0]
 
@@ -100,21 +138,22 @@ print(model2_pile.MohrCoulombPile.getProperties())
 print("\nModel 2 Pile Force Displacement Properties")
 print(model2_pile.ForceDisplacement.getProperties())
 
+# Export data to a csv File
 combined_material_data = {
-    "initial_conditions": model1_initial_conditions,
+    "initialConditions": model1_initial_conditions,
     "stiffness": model1_stiffness,
     "strength": model1_strength,
     "hydraulic": model1_hydraulic,
-    "thermal_conductivity": model1_thermal_conductivity,
-    "thermal_heat_capacity": model1_thermal_heat_capacity,
+    "thermalConductivity": model1_thermal_conductivity,
+    "thermalHeatCapacity": model1_thermal_heat_capacity,
 }
 
 combined_support_data = {
     "bolt": model1_bolt_properties,
     "liner": model1_liner_properties,
     "joint": model1_joint_properties,
-    "pile_interface_properties": model1_pile_interface_properties,
-    "pile_force_displacement_properties": model1_pile_force_displacement_properties,
+    "pileInterfaceProperties": model1_pile_interface_properties,
+    "pileForceDisplacementProperties": model1_pile_force_displacement_properties,
 }
 
 flattened_material_data = {}
@@ -138,6 +177,33 @@ with open(rf"{current_dir}\combined_support_properties.csv", "w", newline="") as
     writer.writerow(["Key", "Value"])
     for key, value in flattened_support_data.items():
         writer.writerow([key, value])
+
+# Import data from csv file to a dictionary
+imported_material_data = {}
+with open(rf"{current_dir}\combined_material_properties.csv", "r") as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        full_key, value = row
+        category, key = full_key.split("_", 1)
+
+        if category not in imported_material_data:
+            imported_material_data[category] = {}
+
+        imported_material_data[category][key] = value
+
+imported_support_data = {}
+with open(rf"{current_dir}\combined_support_properties.csv", "r") as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        full_key, value = row
+        category, key = full_key.split("_", 1)
+
+        if category not in imported_support_data:
+            imported_support_data[category] = {}
+
+        imported_support_data[category][key] = value
 
 model2.saveAs(model2FileSavePath)
 modeler.closeProgram()
